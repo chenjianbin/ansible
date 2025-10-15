bbe4e5e749687b6c4c7f7d238bf32742efbf3601
Your Name <you@example.com>
2025-10-15 22:03:05 +0800

    x

--- roles/zabbix-agent/files/zabbix_agent2.d/mysqlslave/mysql_slave_delay.sh
+++ roles/zabbix-agent/files/zabbix_agent2.d/mysqlslave/mysql_slave_delay.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-TIME=`mysql -u root -S /data0/mysql/$1/mysql.sock -e "show slave status\G" 2> /dev/null|grep -P 'Seconds_Behind_Master'|awk -F\: '{print $2}'|sed s/[[:space:]]//g`
-if [ $TIME = 'NULL' ]
-then 
+
+SOCKET="/data0/mysql/$1/mysql.sock"
+MYSQL="mysql -u root -S $SOCKET"
+
+# 优先尝试 SHOW REPLICA STATUS
+OUTPUT=$($MYSQL -e "SHOW REPLICA STATUS\G" 2>/dev/null)
+if [ -z "$OUTPUT" ]; then
+    OUTPUT=$($MYSQL -e "SHOW SLAVE STATUS\G" 2>/dev/null)
+fi
+
+# 提取延迟字段（高版本 Seconds_Behind_Source，低版本 Seconds_Behind_Master）
+TIME=$(echo "$OUTPUT" | grep -P 'Seconds_Behind_(Master|Source)' | awk -F: '{print $2}' | sed 's/[[:space:]]//g')
+
+# 如果值为 NULL 或空，设置为 1000
+if [ -z "$TIME" ] || [ "$TIME" = "NULL" ]; then
     TIME=1000
 fi
-echo $TIME
+
+echo "$TIME"
--- roles/zabbix-agent/files/zabbix_agent2.d/mysqlslave/mysql_slave_ping.sh
+++ roles/zabbix-agent/files/zabbix_agent2.d/mysqlslave/mysql_slave_ping.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-STATUS=`mysql -u root -S /data0/mysql/$1/mysql.sock -e "show slave status\G" 2>/dev/null|grep -P '(Slave_IO_Running|Slave_SQL_Running)'|grep 'Yes'|wc -l`
-if [ $STATUS = '2' ]
-then	
-	echo 1
-else
-	echo 0
+
+SOCKET="/data0/mysql/$1/mysql.sock"
+MYSQL="mysql -u root -S $SOCKET"
+
+OUTPUT=$($MYSQL -e "SHOW REPLICA STATUS\G" 2>/dev/null)
+if [ -z "$OUTPUT" ]; then
+    OUTPUT=$($MYSQL -e "SHOW SLAVE STATUS\G" 2>/dev/null)
 fi
 
+# 兼容老字段名 (Slave_IO_Running, Slave_SQL_Running) 和新字段名 (Replica_IO_Running, Replica_SQL_Running)
+STATUS=$(echo "$OUTPUT" | grep -P '(Slave_IO_Running|Slave_SQL_Running|Replica_IO_Running|Replica_SQL_Running)' | grep 'Yes' | wc -l)
+
+if [ "$STATUS" = "2" ]; then
+    echo 1
+else
+    echo 0
+fi
